724fd962f989002e98fc7e81eb763e7fc28cae1b
shu_yoshida <C2375@oic.jp>
2025-12-29 23:56:03 +0900

    render-build.shにバンドルバイナリスタブの生成を追加し、データベースのマイグレーションとシードを一度に実行するように変更

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+bundle binstubs bundler --force
 bundle install
 bundle exec rails assets:precompile
-bundle exec rails db:migrate
+bundle exec rails db:migrate db:seed
