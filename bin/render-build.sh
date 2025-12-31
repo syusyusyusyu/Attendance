03128a56593cccf772b1249d3617fc38631679c1
shu_yoshida <C2375@oic.jp>
2025-12-31 19:43:34 +0900

    feat: render-build.shのパフォーマンスを向上させるために、bundle installにオプションを追加

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+export BUNDLE_WITHOUT="${BUNDLE_WITHOUT:-development:test}"
+export BUNDLE_PATH="${BUNDLE_PATH:-/opt/render/project/.gems}"
+
 bundle binstubs bundler --force
-bundle install
+bundle install --jobs=4 --retry=3
 bundle exec rails assets:precompile
 bundle exec rails db:migrate db:seed
