e567fb263fb0d894a7849216dd049759ddc00605
shu_yoshida <C2375@oic.jp>
2025-12-31 19:52:23 +0900

    fix: render-build.shの実行順序を修正し、bundle installの前にbinstubsを生成

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,7 +4,7 @@ set -e
 export BUNDLE_WITHOUT="${BUNDLE_WITHOUT:-development:test}"
 export BUNDLE_PATH="${BUNDLE_PATH:-/opt/render/project/.gems}"
 
-bundle binstubs bundler --force
 bundle install --jobs=4 --retry=3
+bundle binstubs bundler --force
 bundle exec rails assets:precompile
 bundle exec rails db:migrate db:seed
