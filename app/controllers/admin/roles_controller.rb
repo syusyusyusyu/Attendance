572d295d45877a5643108c1aa02a0e5d71eb22fa
shu_yoshida <C2375@oic.jp>
2026-01-12 13:42:39 +0900

    feat: UI/UXポリシーの追加と管理者ロールの更新処理を改善

--- app/controllers/admin/roles_controller.rb
+++ app/controllers/admin/roles_controller.rb
@@ -7,13 +7,18 @@ class Admin::RolesController < Admin::BaseController
   end
 
   def update
-    role = Role.find(params[:id])
+    @roles = Role.includes(:permissions).order(:name)
+    @permissions = Permission.order(:key)
+    @role = Role.find(params[:id])
     attrs = params.require(:role).permit(:label, :description)
     permission_ids = Array(params[:permission_ids]).map(&:to_i)
 
-    role.update(attrs)
-    role.permission_ids = permission_ids
+    @role.update(attrs)
+    @role.permission_ids = permission_ids
 
-    redirect_to admin_roles_path, notice: "権限を更新しました。"
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to admin_roles_path, notice: "権限を更新しました。" }
+    end
   end
 end
