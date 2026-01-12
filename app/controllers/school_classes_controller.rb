a6d118b3c9c2f1fcc3310781cbb3e31131e40039
shu_yoshida <C2375@oic.jp>
2026-01-13 06:58:27 +0900

    feat: クラス作成および更新時にCSVインポート機能を追加し、フォームにカスタム時間指定オプションを実装

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -35,9 +35,13 @@ class SchoolClassesController < ApplicationController
 
     if @school_class.save
       AttendancePolicy.find_or_create_by!(school_class: @school_class, **AttendancePolicy.default_attributes)
+      roster_result = import_roster_if_needed
+      flash_type = roster_result&.fetch(:type, :notice) || :notice
+      flash_message = "クラスを作成しました。"
+      flash_message = "#{flash_message} #{roster_result[:message]}" if roster_result
       respond_to do |format|
-        format.turbo_stream
-        format.html { redirect_to school_class_path(@school_class), notice: "クラスを作成しました。" }
+        format.turbo_stream { flash.now[flash_type] = flash_message }
+        format.html { redirect_to school_class_path(@school_class), flash_type => flash_message }
       end
     else
       render :new, status: :unprocessable_entity
@@ -52,10 +56,14 @@ class SchoolClassesController < ApplicationController
     @school_class.schedule = schedule_params
 
     if @school_class.save
+      roster_result = import_roster_if_needed
       if turbo_frame_request?
         render partial: "school_classes/card", locals: { klass: @school_class }
       else
-        redirect_to school_class_path(@school_class), notice: "クラス情報を更新しました。"
+        flash_type = roster_result&.fetch(:type, :notice) || :notice
+        flash_message = "クラス情報を更新しました。"
+        flash_message = "#{flash_message} #{roster_result[:message]}" if roster_result
+        redirect_to school_class_path(@school_class), flash_type => flash_message
       end
     else
       if turbo_frame_request?
@@ -78,19 +86,8 @@ class SchoolClassesController < ApplicationController
       redirect_to school_class_path(@school_class), alert: "CSVファイルを選択してください。" and return
     end
 
-    result = RosterCsvImporter.new(
-      teacher: current_user,
-      school_class: @school_class,
-      csv_text: file.read
-    ).import
-
-    message = "インポート完了: 新規#{result[:created]}件 / 更新#{result[:updated]}件 / 履修追加#{result[:enrolled]}件"
-    if result[:errors].any?
-      errors = result[:errors].first(3).join(" ")
-      redirect_to school_class_path(@school_class), alert: "#{message} (エラー: #{errors})"
-    else
-      redirect_to school_class_path(@school_class), notice: message
-    end
+    roster_result = build_roster_import_result(file)
+    redirect_to school_class_path(@school_class), roster_result[:type] => roster_result[:message]
   end
 
   private
@@ -119,6 +116,18 @@ class SchoolClassesController < ApplicationController
     start_time = data[:schedule_start_time].presence
     end_time = data[:schedule_end_time].presence
     frequency = data[:schedule_frequency].presence || "weekly"
+    use_custom_time = data[:schedule_use_custom_time].to_s == "1"
+
+    if use_custom_time
+      return {} if day.blank? || start_time.blank? || end_time.blank?
+
+      return {
+        "day_of_week" => day.to_i,
+        "start_time" => start_time,
+        "end_time" => end_time,
+        "frequency" => frequency
+      }
+    end
 
     if period.present?
       times = SchoolClass.period_times(period)
@@ -141,5 +150,28 @@ class SchoolClassesController < ApplicationController
       "end_time" => end_time,
       "frequency" => frequency
     }
+  end
+
+  def import_roster_if_needed
+    file = params[:roster_csv_file]
+    return nil if file.blank?
+
+    build_roster_import_result(file)
+  end
+
+  def build_roster_import_result(file)
+    result = RosterCsvImporter.new(
+      teacher: current_user,
+      school_class: @school_class,
+      csv_text: file.read
+    ).import
+
+    message = "CSVインポート完了: 新規#{result[:created]}件 / 更新#{result[:updated]}件 / 履修追加#{result[:enrolled]}件"
+    if result[:errors].any?
+      errors = result[:errors].first(3).join(" ")
+      return { type: :alert, message: "#{message} (エラー: #{errors})" }
+    end
+
+    { type: :notice, message: message }
   end
 end
